fb7132fa60778ca8a0d3538de2d9b90864113baf
timcoumu <tim2021000196@gmail.com>
2026-08-04 12:19:51 +0800

    [refactor]优化部分变量名，引入kotlin和viewmodel

---
+++ my_books/app/src/main/java/com/example/my_books/activity/register/RegisterViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.my_books.activity.register
+
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.lifecycle.ViewModel
+import com.example.my_books.sql.sql
+
+class RegisterViewModel(sqlHelper: sql,db: SQLiteDatabase) : ViewModel(){
+}
